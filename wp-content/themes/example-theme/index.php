0ab098348b84667aed2341715493cacdbb5920e7
Elina Sankila <sankilaelina@gmail.com>
2024-03-20 15:51:50 +0200

    index muokattu

--- wp-content/themes/example-theme/index.php
+++ wp-content/themes/example-theme/index.php
@@ -2,10 +2,9 @@
 if ( have_posts() ) :
     while ( have_posts() ) :
         the_post();
-        the_title();
+        the_title(before: '<h1>', after: '<h1>');
         the_content();
     endwhile;
 else :
     _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
 endif;
-?>
